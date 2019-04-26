b166b414e9d713338626ba57f3084ab055c5314a
acsirina <irina.a@irinas-MacBook-Pro.local>
2019-04-26 13:21:47 -0400

    edit and delete buttons work

--- models/entries.js
+++ models/entries.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
 const entrySchema = new mongoose.Schema({
-    distance: { type: String},
+    description:{ type: String},
+    distance: { type: Number},
     boat:{ type: String},
     zone:{ type: Number},
-    onWater:{ type: boolean },
-    onLand:{ type: boolean }
+    onWater:{ type: Boolean },
+    onLand:{ type: Boolean }
 },
     {timestamps: true});
 const Entry = mongoose.model ('Entry', entrySchema);
--- server.js
+++ server.js
@@ -6,17 +6,19 @@ const methodOverride  = require('method-override');
 const mongoose = require ('mongoose');
 const app = express ();
 const db = mongoose.connection;
-//___________________
-//Port
+
+//MODEL(it is like a contract)
+const Entry = require('./models/entries.js')
+
+//PORT
 //___________________
 // Allow use of Heroku's port or your own local port, depending on the environment
 const PORT = process.env.PORT || 3000;
 
-//___________________
-//Database
+//DB SETUP
 //___________________
 // How to connect to the database either via heroku or locally
-const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `fruits`;
+const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/'+ `dockit`;
 
 // Connect to Mongo
 mongoose.connect(MONGODB_URI ,  { useNewUrlParser: true});
@@ -30,7 +32,7 @@ db.on('disconnected', () => console.log('mongo disconnected'));
 db.on('open' , ()=>{});
 
 //___________________
-//Middleware
+//MIDDLEWARE
 //___________________
 
 //use public folder for static assets
@@ -43,14 +45,109 @@ app.use(express.json());// returns middleware that only parses JSON - may or may
 //use method override
 app.use(methodOverride('_method'));// allow POST, PUT and DELETE from a form
 
+//___________________
+//ROUTES
+//________________
 
-//___________________
-// Routes
-//___________________
-//localhost:3000
+
+//ROOT
 app.get('/' , (req, res) => {
   res.send('Hello World this is still connected!');
 });
+
+//HOME
+app.get('/home', (req, res)=>{
+    res.render('home.ejs')
+      console.log('home is connected')
+});
+
+//INDEX
+app.get('/dockit', (req, res)=>{
+  Entry.find({}, (error, entry)=>{
+      // console.log(Entry)
+      res.render('index.ejs', {
+          entry: entry
+      });
+  });
+});
+
+
+// NEW (CLIENT)
+app.get('/dockit/new', (req, res) => {
+  res.render('new.ejs')
+})
+
+// SHOW 
+app.get('/dockit/:id', (req, res)=>{
+  Entry.findById(req.params.id, (err, entry)=>{
+      console.log(req.params.id)
+      res.render('show.ejs', {
+          entry:entry
+      });
+  });
+});
+
+//CREATE
+app.post('/dockit', (req, res) => {
+  if(req.body.zone === 1){ 
+      return('great job')
+  } 
+  Entry.create(req.body, (error, entry)=>{
+      if (error) {
+          res.send(error)
+      } else {
+          res.redirect('/dockit');
+      }
+  });
+})
+// DELETE
+app.delete('/dockit/:id', (req, res) => {
+  Entry.findByIdAndRemove(req.params.id, (err, deletedEntry)=>{
+      res.redirect('/dockit');//redirect back to entries index
+  });
+})
+// EDIT (CLIENT)
+app.get('/dockit/:id/edit', (req, res)=>{
+  Entry.findById(req.params.id, (err, foundEntry)=>{ //find the entry
+      res.render('edit.ejs', {
+          entry: foundEntry //pass in found entry
+      });
+  });
+});
+// UPDATE (SERVER)
+app.put('/dockit/:id', (req, res)=>{
+  
+  Entry.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err, updatedEntry)=>{
+      res.redirect('/dockit');
+  });
+});
+
+// app.get('/seed', async (req, res) => {
+//     const newEntry =
+      
+//     [
+//       {
+//         distance: '12',
+//         description:'We did 5 x 12 mins @22 with 5 mins rest time',
+//         zone: '5',
+
+        
+//       }, {
+//         distance: '12',
+//         description:'We did 5 x 12 mins @22 with 5 mins rest time',
+//         zone: '5',
+        
+//       }
+//     ]
+
+
+// try {
+//   const seedItems = await Entry.create(newEntry)
+//   res.send(seedItems)
+// } catch (err) {
+//   res.send(err.message)
+// }
+// })
 
 //___________________
 //Listener
